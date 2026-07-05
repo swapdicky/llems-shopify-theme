76f9caf18874fb307ed9973d1bd1ff32eba31110
swapdicky <dw@swwwap.hk>
2026-07-05 08:02:27 +0100

    Initial Shopify theme

--- assets/pubsub.js
+++ assets/pubsub.js
@@ -1,25 +1,23 @@
-let subscribers = {};
+let subscribers = {}
 
 function subscribe(eventName, callback) {
   if (subscribers[eventName] === undefined) {
-    subscribers[eventName] = [];
+    subscribers[eventName] = []
   }
 
   subscribers[eventName] = [...subscribers[eventName], callback];
 
   return function unsubscribe() {
     subscribers[eventName] = subscribers[eventName].filter((cb) => {
-      return cb !== callback;
+      return cb !== callback
     });
-  };
-}
+  }
+};
 
 function publish(eventName, data) {
   if (subscribers[eventName]) {
-    const promises = subscribers[eventName]
-      .map((callback) => callback(data))
-    return Promise.all(promises);
-  } else {
-    return Promise.resolve()
+    subscribers[eventName].forEach((callback) => {
+      callback(data)
+    })
   }
 }
